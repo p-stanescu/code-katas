ebf2f6ea187dd00022ade6e932e994c3177ce9ec
Petru <petru@Mac.lan>
2025-02-03 20:07:41 +0000

    final setup

---
+++ eslint.config.js
@@ -0,0 +1,21 @@
+export default [
+  {
+    ignores: ["node_modules", "dist", "coverage", "jest.config.ts"],
+  },
+  {
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+    },
+    plugins: {
+      "@typescript-eslint": require("@typescript-eslint/eslint-plugin"),
+    },
+    rules: {
+      "no-unused-vars": "warn",
+      "no-console": ["warn", { allow: ["warn", "error"] }],
+      "@typescript-eslint/no-explicit-any": "error",
+      "@typescript-eslint/no-unused-vars": "error",
+      "@typescript-eslint/explicit-function-return-type": "error",
+    },
+  },
+];
