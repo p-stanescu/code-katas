a75dee3395391a552953a96671788f47381157d6
Petru <mi18@protonmail.com>
2025-08-28 20:59:53 +0100

    wip

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,20 @@
 import prettier from 'eslint-config-prettier';
+import sonarjs from 'eslint-plugin-sonarjs';
 import tseslint from 'typescript-eslint';
 
 export default [
-  { ignores: ['node_modules', 'dist', 'coverage', 'jest.config.ts'] },
+  {
+    ignores: [
+      'node_modules',
+      'dist',
+      'coverage',
+      'jest.config.ts',
+      '*.config.cjs',
+      'util-scripts/**',
+      '.vscode',
+      '.env.local',
+    ],
+  },
 
   // Recommended TypeScript rules (ref: https://typescript-eslint.io/getting-started/)
   ...tseslint.configs.recommended,
@@ -10,10 +22,27 @@ export default [
   // Optional stricter overrides and rules
   {
     files: ['**/*.ts', '**/*.tsx'],
+    plugins: {
+      sonarjs,
+    },
     rules: {
+      // Typescript rules
       '@typescript-eslint/no-explicit-any': 'error',
       '@typescript-eslint/no-unused-vars': 'error',
       '@typescript-eslint/explicit-function-return-type': 'error',
+
+      // Function size rules
+      'max-depth': ['warn', 4],
+      'max-params': ['warn', 4],
+      'max-statements': ['warn', 30],
+      'max-lines-per-function': [
+        'warn',
+        { max: 80, skipBlankLines: true, skipComments: true },
+      ],
+
+      // Complexity rules
+      complexity: ['error', { max: 10 }],
+      'sonarjs/cognitive-complexity': ['warn', 20],
     },
   },
 
